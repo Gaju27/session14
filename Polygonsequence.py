fb0a324f7e72bad661e86349e2911c77a1331357
Gajanana Ganjigatti <40160330+Gaju27@users.noreply.github.com>
2020-11-11 20:18:14 +0530

    Add files via upload

---
+++ Polygonsequence.py
@@ -0,0 +1,44 @@
+
+from Polygonclass import Polygon
+class Polygons:
+    def __init__(self, m, R):
+        if m < 3:
+            raise ValueError('m must be greater than 3')
+        self._m = m
+        self._R = R
+#         self._polygons = []
+        
+    def __len__(self):
+        return self._m - 2
+    
+    def __repr__(self):
+        return f'Polygons(m={self._m}, R={self._R})'
+    
+#     def __getitem__(self, s):
+#         return self._polygons[s]
+        
+    def __iter__(self):
+        return self.PolygonIterator(self)
+    
+    @property
+    def max_efficiency_polygon(self):
+        sorted_polygons = sorted(self,key=lambda p: p.area/p.perimeter,
+                                reverse=True)
+        return sorted_polygons[0]
+        
+    class PolygonIterator:
+        def __init__(self, poly_obj):
+            self._poly_obj = poly_obj
+            self._index = 3
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+#             print('Calling __next__')
+            if self._index > self._poly_obj._m:
+                raise StopIteration
+            else:
+                item = Polygon(self._index,self._poly_obj._m)
+                self._index += 1
+                return item
